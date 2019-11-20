4badfb5636591a17114b671e566c8967d3e47674
Scott Payton <spayton.c@gmail.com>
2019-11-19 20:22:54 -0700

    Fix async functions to return data

--- sandbox.js
+++ sandbox.js
@@ -7,7 +7,24 @@ async function fetchAsync() {
   return posts;
 }
 
-// This is how we use our async function
-fetchAsync()
-    .then(data => console.log(data.results[0].geometry))
-    .catch(reason => console.log(reason.message))
+// function that returns the lat long coords
+async function coordinates(){
+  var data = await fetchAsync()  //invoke the function
+  var coords = data.results[0].geometry.location
+return coords;
+};
+
+// pass the coords into a darksky function
+async function darksky(){
+  // var x = Promise.resolve(coordinates()); //resolver function
+  var x = await coordinates();
+    // x.then(function(v){
+    // var latty = v.lat;
+    // var longyy = v.lng;
+    // console.log(latty);
+    let response = await fetch(`https://api.darksky.net/forecast/a8575d384c0eee696e336415e7548404/${x.lat},${x.lng}`);
+    let weather = await response.json();
+    return console.log(weather);
+};
+
+darksky();
