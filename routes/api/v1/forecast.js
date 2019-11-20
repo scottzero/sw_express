fc7b4df50b02f3dac23653d31e1b70ebd7c74f91
Scott Payton <spayton.c@gmail.com>
2019-11-20 13:14:39 -0700

    Need to create user and faves

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -5,28 +5,28 @@ var router= express.Router();
 
 // var router = express.Router();
 // this function will return our google api coordinates
-async function fetchAsync() {
-  let response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=denver,CO&key=AIzaSyAlLNOLsxFHy8B6WJ7p0LoiNfMCDomBcJg');
+async function fetchAsync(city) {
+  let response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyAlLNOLsxFHy8B6WJ7p0LoiNfMCDomBcJg`);
   let posts = await response.json();
   return posts;
 }
 
 // function that returns the lat long coords
-async function coordinates(){
-  var data = await fetchAsync()  //invoke the function
+async function coordinates(city){
+  var data = await fetchAsync(city)  //invoke the function
   var coords = data.results[0].geometry.location
 return coords;
 };
 
 // pass the coords into a darksky function
-async function darksky(){
-  var x = await coordinates();
+async function darksky(city){
+  var x = await coordinates(city);
     let response = await fetch(`https://api.darksky.net/forecast/a8575d384c0eee696e336415e7548404/${x.lat},${x.lng}`);
     let weather = await response.json();
     return weather;
 };
 router.get("/", (request, response) => {
-  darksky()
+  darksky(request.query.location)
   .then(data => response.send(data))
   .catch(reason => response.send(reason.message))
 });
