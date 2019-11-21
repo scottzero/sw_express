5c7494b9277428cb62601a1a7f58ae6f0af4206c
Scott Payton <spayton.c@gmail.com>
2019-11-21 08:37:59 -0700

    Clean up app module

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Requirements
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -5,10 +6,10 @@ const logger = require('morgan');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const indexRouter = require('./routes/index');
-const forecastRouter = require('./routes/api/v1/forecast'); //require forecast controller
-const favoritesRouter = require('./routes/api/v1/favorites'); //require forecast controller
+const forecastRouter = require('./routes/api/v1/forecast');
+const favoritesRouter = require('./routes/api/v1/favorites'); 
 const app = express();
-
+//Exress.uses
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,4 +19,5 @@ app.use('/', indexRouter);
 app.use('/',indexRouter);
 app.use('/api/v1/forecast',forecastRouter.router);//endpoint for forecasts
 app.use('/api/v1/favorites',favoritesRouter);//endpoint for favorites
+//Exports
 module.exports = app;
