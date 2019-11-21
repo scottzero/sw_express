ccc21c7686191ac7668e1bd55dc791be9ad73509
Scott Payton <spayton.c@gmail.com>
2019-11-20 17:46:27 -0700

    Add loop to get al favorite locations into a array

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -6,3 +6,22 @@ var router= express.Router();
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
+
+async function getUser(key){
+  let users = await database('users').where('api_key', key);
+  return (users[0].id);
+}
+async function getFaves(key){
+  let u_id = await getUser(key);
+  let location = await database('favorites').where('user_id', u_id);
+  // let result = [];
+  var faves = location.map(x => x.location)
+  return console.log(faves);
+   // result.push(i.location);
+  // return console.log(result);
+}
+
+router.get("/", (request, response) => {
+  getFaves(request.body.api_key)
+});
+module.exports = router;
