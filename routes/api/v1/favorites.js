0aba1c071225643e32a258a0a044a07c0e27a85a
Scott Payton <spayton.c@gmail.com>
2019-11-21 08:48:58 -0700

    Clean up favorites module

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,3 +1,4 @@
+//Requirements
 const env = require('dotenv').config();
 const fetch = require('node-fetch');
 const express = require('express');
@@ -7,22 +8,22 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const forecast = require('./forecast')
+//Functions
+  async function getUser(key){
+    let users = await database('users').where('api_key', key);
+    return (users[0].id);
+  }
+  async function getFaves(key){
+    let u_id = await getUser(key);
+    let location = await database('favorites').where('user_id', u_id);
+    var faves = await location.map(x => x.location)
+    return await forecast.darksky(faves[0]);
+  }
 
-
-async function getUser(key){
-  let users = await database('users').where('api_key', key);
-  return (users[0].id);
-}
-async function getFaves(key){
-  let u_id = await getUser(key);
-  let location = await database('favorites').where('user_id', u_id);
-  var faves = await location.map(x => x.location)
-  return await forecast.darksky(faves[0]);
-}
-
-router.get("/", (request, response) => {
-  getFaves(request.body.api_key)
-  .then(data => response.status(200).send(data)) //sends to render out data
-  .catch(reason => response.send(reason.message));
-});
+  router.get("/", (request, response) => {
+    getFaves(request.body.api_key)
+    .then(data => response.status(200).send(data)) //sends to render out data
+    .catch(reason => response.send(reason.message));
+  });
+//Exports
 module.exports = router;
